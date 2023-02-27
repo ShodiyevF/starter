801a874f23a7082fdb7dbd2e7250899c0cf0b7d3
Fayzulloh <71442024+ShodiyevF@users.noreply.github.com>
2023-02-27 18:19:37 +0500

    add create user api

--- src/modules/users/users.ctrl.js
+++ src/modules/users/users.ctrl.js
@@ -9,6 +9,11 @@ async function createUserCtrl(req, res) {
     const users = await createUserCtrl(req.body);
     if(users.action == false){
         return res.json(users)
+    } else {
+        return res.json({
+            status: 200,
+            message: 'User has created'
+        })
     }
 }
 
--- src/modules/users/users.index.js
+++ src/modules/users/users.index.js
@@ -1,7 +1,8 @@
-const { getUsersCtrl } = require('./users.ctrl');
+const { getUsersCtrl, createUserCtrl } = require('./users.ctrl');
 
 const express = require('express').Router();
 
-express.get('/users', (req, res) => getUsersCtrl(req, res));
+express.get('/api/users', (req, res) => getUsersCtrl(req, res));
+express.post('/api/users', (req, res) => createUserCtrl(req, res));
 
 module.exports = express;
