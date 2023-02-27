4507783a72501baed5027b67ed7c5180408d5c1b
Fayzulloh <71442024+ShodiyevF@users.noreply.github.com>
2023-02-27 18:15:15 +0500

    users create model

--- src/modules/users/users.model.js
+++ src/modules/users/users.model.js
@@ -6,6 +6,20 @@ async function getUsersModel() {
     return users.rows;
 }
 
+async function createUserModel(body) {
+    const user = await uniqRow('select * from users where user_username = $1', body.username)
+
+    if(user.rows.length){
+        return {
+            action: false,
+            status: 400,
+            message: 'user already exists'
+        }
+    }
+    
+    await uniqRow(`insert into users (user_fullname, user_username, user_password) values ($1,$2,$3)`);
+}
+
 module.exports = {
     getUsersModel,
 };
